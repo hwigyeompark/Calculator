362c644ef512c31419d23d3a3c92afd3f3d98c24
hwigyeompark <phg5590@gmail.com>
2018-05-01 15:17:27 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ContactForm from './Component/ContactForm';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <ContactForm />
+      </div>
+    );
+  }
+}
+
+export default App;
