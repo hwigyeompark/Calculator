babd5ef53f4175bc3b21b29cc5ec82823c15852a
hwigyeompark <phg5590@gmail.com>
2018-05-02 21:21:53 +0900

    update calculator

--- src/Component/calculate.js
+++ src/Component/calculate.js
@@ -14,14 +14,12 @@ export default function calculate(obj, buttonName) {
         if (buttonName === '0' && obj.next === '0') {
             return {};
         }
-        // If there is an operation, update next
         if (obj.operation) {
             if (obj.next) {
                 return { next: obj.next + buttonName };
             }
             return { next: buttonName };
         }
-        // If there is no operation, update next and clear the value
         if (obj.next) {
             return {
                 next: obj.next + buttonName,
@@ -61,7 +59,6 @@ export default function calculate(obj, buttonName) {
                 operation: null,
             };
         } else {
-            // '=' with no operation, nothing to do
             return {};
         }
     }
@@ -76,15 +73,6 @@ export default function calculate(obj, buttonName) {
         return {};
     }
 
-    // Button must be an operation
-
-    // When the user presses an operation button without having entered
-    // a number first, do nothing.
-    // if (!obj.next && !obj.total) {
-    //   return {};
-    // }
-
-    // User pressed an operation button and there is an existing operation
     if (obj.operation) {
         return {
             total: operate(obj.total, obj.next, obj.operation),
@@ -93,14 +81,10 @@ export default function calculate(obj, buttonName) {
         };
     }
 
-    // no operation yet, but the user typed one
-
-    // The user hasn't typed a number yet, just save the operation
     if (!obj.next) {
         return { operation: buttonName };
     }
-
-    // save the operation and shift 'next' into 'total'
+    
     return {
         total: obj.next,
         next: null,
